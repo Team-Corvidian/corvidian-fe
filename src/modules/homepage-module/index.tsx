53d31d19129b44e2d390ce73f136e1317fab0cd2
Grace Karina <gracekarina12@gmail.com>
2025-10-03 19:40:43 +0700

    feat: add project section to homepage module

--- src/modules/homepage-module/index.tsx
+++ src/modules/homepage-module/index.tsx
@@ -1,11 +1,13 @@
 import { HeroSection } from "./section/hero-section";
 import { ProblemSection } from "./section/problem-section";
+import { ProjectSection } from "./section/project-section";
 
 export const HomepageModule = () => {
   return (
     <div className="w-full min-h-screen">
       <HeroSection />
       <ProblemSection />
+      <ProjectSection />
     </div>
   );
 }
