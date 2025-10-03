70c10f4958b3627875dedb7ac69943ab14280d9a
Grace Karina <gracekarina12@gmail.com>
2025-10-03 11:26:54 +0700

    feat: update homepage layout with full-width and minimum height

--- src/modules/homepage-module/index.tsx
+++ src/modules/homepage-module/index.tsx
@@ -1,9 +1,11 @@
 import { HeroSection } from "./section/hero-section";
+import { ProblemSection } from "./section/problem-section";
 
 export const HomepageModule = () => {
   return (
-    <div>
+    <div className="w-full min-h-screen">
       <HeroSection />
+      <ProblemSection />
     </div>
   );
 }
