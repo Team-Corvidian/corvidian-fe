48561bf6203d78f1176d59244f23c8d153a9b9f9
Grace Karina <gracekarina12@gmail.com>
2025-10-05 17:01:34 +0700

    feat: enhance desktop card layout and responsiveness

--- src/modules/homepage-module/components/problem/desktop-card.tsx
+++ src/modules/homepage-module/components/problem/desktop-card.tsx
@@ -3,28 +3,28 @@ import Link from "next/link";
 
 export const DesktopCard = () => {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+    <div className="grid grid-cols-1 lg:grid-cols-[1.2fr_1fr_1fr] max-lg:grid-cols-[1.4fr_1.2fr_1fr] xl:grid-cols-[2.1fr_1.4fr_1fr] 2xl:grid-cols-[2.4fr_1.4fr_1fr] gap-6 lg:max-h-[480px] max-lg:max-h-[300px]">
       <div className="flex flex-col gap-6">
-        <div className="bg-gradient-to-br from-corvidian-3 to-white rounded-3xl">
+        <div className="bg-gradient-to-br from-corvidian-3 to-white rounded-3xl lg:max-h-[500px] max-lg:w-full overflow-hidden">
           <Image
             src="/hero/modem.png"
             alt="Modem"
             width={633}
             height={307}
-            className="w-full h-auto rounded-t-3xl"
+            className="lg:w-full h-[307px] rounded-t-3xl max-lg:w-[270px] max-lg:h-[170px] max-lg:ml-auto max-lg:-translate-y-3"
           />
-          <div className="px-8 -mt-20 ">
-            <p className="text-corvidian-1 font-normal text-sm mb-4">
+          <div className="px-8 -mt-28 md:-translate-y-7 max-lg:-mt-14">
+            <p className="text-corvidian-1 font-normal text-sm max-lg:text-[10px]">
               Konsultasikan supaya bisnis Anda nggak <br /> kena risiko
               downtime.
             </p>
-            <div className="flex justify-between h-[80px]">
-              <p className="text-corvidian-1 font-extrabold text-xl">
+            <div className="flex justify-between h-[80px] lg:h-[76px] items-end md:h-[40px]">
+              <p className="text-corvidian-1 font-extrabold 2xl:text-2xl lg:text-xl max-lg:text-[11px]">
                 Sistem IT sering error?
                 <br />
                 Jaringan lambat?
               </p>
-              <p className="text-corvidian-1 font-bold text-sm underline translate-y-8 cursor-pointer">
+              <p className="text-corvidian-1 font-bold text-md underline cursor-pointer max-lg:text-sm max-lg:text-[11px]">
                 Pelajari lebih lanjut
               </p>
             </div>
@@ -33,33 +33,33 @@ export const DesktopCard = () => {
 
         <div className="bg-[linear-gradient(91.4deg,#1D1F26_1.19%,#1D1F26_53.64%,#1578CB_157.04%)] rounded-3xl p-8 max-h-[154px]">
           <div className="flex flex-col justify-between">
-            <p className="text-white font-extrabold text-lg lg:text-lg whitespace-nowrap">
+            <p className="text-corvidian-3 font-extrabold text-2xl lg:text-lg whitespace-nowrap 2xl:text-3xl max-lg:text-[10px]">
               Mau buat aplikasi berkualitas dan terjangkau?
               <br />
               Konsultasikan dengan kami
             </p>
-            <p className="text-white font-bold text-sm underline text-right cursor-pointer">
+            <p className="text-corvidian-5 font-bold text-md underline text-right cursor-pointer max-lg:text-[10px] max-lg:mt-2">
               Pelajari lebih lanjut
             </p>
           </div>
         </div>
       </div>
 
-      <div className="bg-[linear-gradient(339.7deg,#FFFFFF_-14%,#C5CED5_94.78%)] rounded-3xl p-8">
-        <div className="flex flex-col justify-between h-full min-h-[400px]">
+      <div className="bg-[linear-gradient(339.7deg,#FFFFFF_-14%,#C5CED5_94.78%)] rounded-3xl p-8 max-lg:p-4">
+        <div className="flex flex-col justify-between lg:h-full min-h-[400px] max-lg:min-h-[200px]">
           <div>
-            <h3 className="font-extrabold text-2xl 2xl:text-3xl mb-4 bg-[linear-gradient(90.58deg,#02C2B3_-13.4%,#1D1F26_25.6%,#1D1F26_63.53%,#1578CB_116.56%)] bg-clip-text text-transparent">
-              Website kamu terlihat kurang profesional? <br /> dan lambat
-              bikin calon <br /> klien kabur?
+            <h3 className="font-extrabold max-lg:text-sm lg:text-xl text-2xl 2xl:text-3xl mb-4 bg-[linear-gradient(90.58deg,#02C2B3_-13.4%,#1D1F26_25.6%,#1D1F26_63.53%,#1578CB_116.56%)] bg-clip-text text-transparent">
+              Website kamu terlihat kurang profesional? <br /> dan lambat bikin
+              calon <br /> klien kabur?
             </h3>
-            <p className="text-corvidian-1 font-normal text-sm 2xl:text-lg">
-              Saatnya buat website yang cepat, responsif, nyaman dipake
-              dengan tampilan yang lebih meyakinkan dan dipersi aja.
+            <p className="text-corvidian-1 font-normal text-sm 2xl:text-lg mt-8 max-lg:text-[10px]">
+              Saatnya buat website yang cepat, responsif, nyaman dipake dengan
+              tampilan yang lebih meyakinkan dan dipersi aja.
             </p>
           </div>
           <Link
             href="/service"
-            className="text-corvidian-1 font-bold text-sm underline text-left"
+            className="text-corvidian-1 font-bold text-lg underline text-left translate-y-2 max-lg:text-[10px] max-lg:pt-20"
           >
             Pelajari lebih lanjut
           </Link>
@@ -73,21 +73,19 @@ export const DesktopCard = () => {
             alt="Building"
             width={263}
             height={200}
-            className="w-full max-h-[300px] rounded-t-3xl"
+            className="w-full max-h-[300px] max-lg:max-h-[100px] rounded-t-3xl"
           />
-          <div className="p-8 flex flex-col justify-between flex-grow lg:-mt-40">
+          <div className="px-8 max-lg:px-6 flex flex-col justify-between flex-grow -translate-y-16 max-lg:-translate-y-4">
             <div>
-              <h3 className="text-white font-extrabold text-xl 2xl:text-3xl mb-4">
-                Tidak punya waktu & budget <br /> buat bangun sistem dari
-                nol?
+              <h3 className="text-white font-extrabold text-2xl lg:text-xl mb-4 max-lg:text-sm">
+                Tidak punya <br className="2xl:block hidden" /> waktu & budget{" "}
+                <br /> buat bangun sistem dari nol?
               </h3>
-              <p className="text-white font-normal text-sm 2xl:text-xl mb-4">
+              <p className="text-white font-normal text-sm 2xl:text-md mb-8 max-lg:text-[10px]">
                 Co-Pilot siap digunakan dalam hitungan minggu, bukan bulan.
               </p>
             </div>
-            <p
-              className="text-white font-bold text-sm underline text-left cursor-pointer"
-            >
+            <p className="text-white font-bold text-md underline text-left cursor-pointer lg:pt-11 xl:pt-3 max-lg:text-[10px] max-lg:mb-4">
               Pelajari lebih lanjut
             </p>
           </div>
