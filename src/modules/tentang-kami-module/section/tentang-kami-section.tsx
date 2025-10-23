5a845098659e0eec8b5f7f255054c55c9b1cc475
Grace Karina <gracekarina12@gmail.com>
2025-10-23 22:54:15 +0700

    feat: replace header

--- src/modules/tentang-kami-module/section/tentang-kami-section.tsx
+++ src/modules/tentang-kami-module/section/tentang-kami-section.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Header } from '../components/header'
 import { ArtikelKami } from '../components/artikel-kami'
 import { ImageSection } from '../components/image';
 import MengapaRekamJejak from '../components/mengapa-rekam-jejak';
@@ -8,12 +7,13 @@ import TimKami from '../components/tim-kami';
 import CaraKamiBekerja from '../components/cara-kami-bekerja';
 import ConsultationSection from '@/modules/homepage-module/section/consultation-section';
 import ConsultationSchedule from '@/modules/homepage-module/section/schedule-consultation-section';
+import HeroSectionTentangKami from '../components/hero-section-tentang-kami';
 
 
 const TentangKamiSection = () => {
   return (
     <section>
-      <Header />
+      <HeroSectionTentangKami />
       <ImageSection />
       <MengapaRekamJejak />
       <Solusi />
