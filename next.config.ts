ca3d5c15bc9a9e9f6e5b3186d56434cc77419d85
Grace Karina <gracekarina12@gmail.com>
2025-11-11 22:33:48 +0700

    refactor: streamline remote patterns configuration in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,33 @@
 import type { NextConfig } from "next";
+import type { RemotePattern } from "next/dist/shared/lib/image-config";
+
+const apiUrl = process.env.NEXT_PUBLIC_API_BASE_URL
+  ? new URL(process.env.NEXT_PUBLIC_API_BASE_URL)
+  : null;
+
+const remotePatterns: RemotePattern[] = [
+  {
+    protocol: "http",
+    hostname: "127.0.0.1",
+  },
+  {
+    protocol: "http",
+    hostname: "localhost",
+  },
+];
+
+if (apiUrl) {
+  const protocol = apiUrl.protocol.replace(":", "") as "http" | "https";
+
+  remotePatterns.push({
+    protocol,
+    hostname: apiUrl.hostname,
+  });
+}
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: '127.0.0.1',
-      },
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-      },
-    ],
-    qualities: [75, 90, 100],
+    remotePatterns,
   },
 };
 
